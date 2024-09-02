b2ac3da3683fba53651065681cb3507ea6717cb7
walidbnslimane <164651966+walidbnslimane@users.noreply.github.com>
2024-09-02 14:21:09 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const textArray = ["Web Developer", "Mobile Developer", "Game Developer", "3D Designer", "Roboticist"];
+const typingText = document.querySelector("#typing-text"); 
+const staticText = document.querySelector(".static-text"); 
+let textIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textIndex].length) {
+        typingText.textContent += textArray[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(erase, 2000);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typingText.textContent = typingText.textContent.slice(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, 50);
+    } else {
+        textIndex = (textIndex + 1) % textArray.length;
+        setTimeout(type, 500);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(type, 1000);
+});
